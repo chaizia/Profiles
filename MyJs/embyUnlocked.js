4dac0b7def338ceceea0ffeb13288c870dc48a7b
chaizi <chaizia@gmail.com>
2023-09-06 14:36:22 +0800

    Update embyUnlocked.js

--- MyJs/embyUnlocked.js
+++ MyJs/embyUnlocked.js
@@ -1,62 +1,10 @@
-/**
- * @author: KleinerSource
- *
- * @fileoverview Example to compose response for rewrite of script-echo-response.
- *
- * $request.url, $notify(title, subtitle, message), console.log(message), $done(response)
- *
- * @supported Quantumult X (v1.0.3-build141)
- */
-var url = $request.url;
-
-const myStatus = "HTTP/1.1 200 OK";
-const myHeaders = {
-  Crack: "KS",
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers": "*",
-  "Access-Control-Allow-Method": "*",
-  "Access-Control-Allow-Credentials": "true",
-};
-
-if (url.indexOf("/admin/service/registration/validateDevice") != -1) {
-  obj = {
-    cacheExpirationDays: 365,
-    message: "Device Valid",
-    resultCode: "GOOD",
-  };
-} else if (url.indexOf("/admin/service/appstore/register") != -1) {
-  obj = {
-    featId: "",
-    registered: true,
-    expDate: "2099-01-01",
-    key: "",
-  };
-} else if (url.indexOf("/admin/service/registration/validate") != -1) {
-  obj = {
-    featId: "",
-    registered: true,
-    expDate: "2099-01-01",
-    key: "",
-  };
-} else if (url.indexOf("/admin/service/registration/getStatus") != -1) {
-  obj = {
-    planType: "Cracked",
-    deviceStatus: "",
-    subscriptions: [],
-  };
-} else if (url.indexOf("/admin/service/supporter/retrievekey") != -1) {
-  obj = {
-    Success: false,
-    ErrorMessage: "Supporter not found",
-  };
+if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
+    if($response.status!=200){
+        $notification.post("Emby Premiere 已激活", "", "");
+        $done({status: 200, headers: $response.headers, body: '{"cacheExpirationDays":999,"resultCode":"GOOD","message":"Device Valid"}' })
+    }else{
+        $done({})
+    }
+}else{
+    $done({})
 }
-
-myData = JSON.stringify(obj);
-
-const myResponse = {
-  status: myStatus,
-  headers: myHeaders, // Optional.
-  body: myData, // Optional.
-};
-
-$done(myResponse);
